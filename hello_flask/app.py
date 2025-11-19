32e11eb25591ecc806fab448780f2eaaedd67f88
EnamulRahman <e.r.200@hotmail.co.uk>
2025-11-19 08:44:59 +0000

    add all learning

---
+++ hello_flask/app.py
@@ -0,0 +1,22 @@
+# app.py
+from flask import Flask
+import MySQLdb
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    # Connect to the MySQL database
+    db = MySQLdb.connect(
+        host="db",    # Changed from "host.docker.internal" to "db" (service name)
+        user="root",
+        passwd="my-secret-pw",
+        db="mysql"
+    )
+    cur = db.cursor()
+    cur.execute("SELECT VERSION()")
+    version = cur.fetchone()
+    return f'Hello, World! MySQL version: {version[0]}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
